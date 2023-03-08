7c127766243cc53dadc18d8c88b9c02a67f07bbf
Vladislav Misko <vladislavmisko@Vladislavs-MacBook-Pro.local>
2023-03-08 17:55:28 +0300

    added dummy crossover

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let population = []
-const POPULATION_VOLUME = 500
+const POPULATION_VOLUME = 10
 const INITIAL_INDIVIDUAL = "ACTGACTGACTGACTG"
 const GENES = ['A', 'C', 'T', 'G']
 
 function init() {
+  console.log('initializing population...')
   for (let i = 0; i < POPULATION_VOLUME; i++) {
     population.push(INITIAL_INDIVIDUAL)
   }
@@ -14,13 +15,12 @@ function mutation() {
   let mutated_population = []
   
   for (let i = 0; i < population.length; i++) {
-    let mutated_individual = population[i]
-    
-    let mutated_gene = Math.floor(Math.random() * mutated_individual.length)
+    let mutated_gene = Math.floor(Math.random() * population[i].length)
     let mutation_value = GENES[Math.floor(Math.random() * GENES.length)]
-    mutated_individual[mutated_gene] = mutation_value
+    let mutated_individual = population[i].substring(0, mutated_gene) + mutation_value + population[i].substring(mutated_gene + 1)
     
     mutated_population.push(mutated_individual)
+    console.log(`${population[i]} mutated to ${mutated_individual}`)
   }
 
   for (let i = 0; i < mutated_population.length; i++) {
@@ -35,8 +35,41 @@ function fitness(individual) {
   return A_count / individual.length
 }
 
+function cross_a_b(A, B) {
+  // TODO: compute min length
+
+  let children = '';
+
+  for (let i = 0; i < A.length; i++) {
+    if (Math.random < 0.5) {
+      children += A[i]
+    } else {
+      children += B[i]
+    }
+  }
+  
+  return children
+}
+
 function crossover() {
   console.log('Executing crossover...')
+  population.sort((a, b) => fitness(a) - fitness(b))
+
+  let children = []
+
+  // TODO: add X best & Y worst
+  for (let i = 0; i < population.length; i += 2) {
+    let child = cross_a_b(population[i], population[i+1])
+    console.log(`parents: ${population[i]}, ${population[i+1]} -> child: ${child}`)
+
+    children.push(child)
+  }
+
+  for (let i = 0; i < children.length; i++) {
+    population.push(children[i])
+  }
+
+  console.log(`new population length: ${population.length}`)
 }
 
 function selection() {
@@ -59,6 +92,7 @@ function loop(iterations) {
 
 function main() {
   console.log('Hello there! This is genetic alghorithm demo.')
+  init()
   loop(5)
 }
 
